63e3758b43ac29ffcbaa0c490a5479b7f780c6c6
Heron Yang <heron.yang.tw@gmail.com>
2015-10-02 16:57:15 +0800

    add minimalism prototype

--- minimalism/main.js
+++ minimalism/main.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+var t = $('#t');
+
+function tick() {
+    var d = new Date();
+    var h = ('0' + d.getHours()).slice(-2);
+    var m = ('0' + d.getMinutes()).slice(-2);
+    var s = ('0' + d.getSeconds()).slice(-2);
+    // var m = d.getMinutes();
+    // var s = d.getSeconds();
+
+    var t_to_show = h + " " + m + " " + s;
+    t.text(t_to_show);
+    document.title = t_to_show;
+}
+
+$(document).ready(function() {
+    (function(){
+            tick();
+            setTimeout(arguments.callee, 500);
+    })();
+});
